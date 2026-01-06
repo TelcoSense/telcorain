033cbe8b6e440e1868f6d4907aa4730627e63aad
xistva02 <istvanek.matej.com>
2026-01-06 23:43:57 +0100

    update cython comp for linux distro

--- telcorain/cython/setup.py
+++ telcorain/cython/setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup
+from __future__ import annotations
+
+from pathlib import Path
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
-setup(
-    ext_modules=cythonize(
-      	"raincolor.pyx",
-        compiler_directives={"language_level": "3"}
-    ),
+HERE = Path(__file__).resolve().parent
+ROOT = HERE.parent.parent
+PYX = HERE / "raincolor.pyx"
+
+ext = Extension(
+    name="telcorain.cython.raincolor",
+    sources=[str(PYX)],
     include_dirs=[np.get_include()],
+    define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+)
+
+setup(
+    name="telcorain",
+    ext_modules=cythonize(
+        [ext],
+        compiler_directives={"language_level": "3"},
+    ),
 )
